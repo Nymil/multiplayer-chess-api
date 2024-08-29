e45cf522f360588c507f319cc406e5fb2a798488
Mattice Heyzak <mattice.heyzak@student.howest.be>
2024-08-29 21:04:59 +0200

    adding of starting pieces

--- Logic/Domain/Board.cs
+++ Logic/Domain/Board.cs
@@ -24,7 +24,31 @@ namespace Logic.Domain
 
         private void AddStartPieces()
         {
-            return;
+            AddNonPawnPiecesForColor(PlayerColor.White);
+            AddNonPawnPiecesForColor(PlayerColor.Black);
+            AddNonPawnPieces();
+        }
+
+        private void AddNonPawnPiecesForColor(PlayerColor color)
+        {
+            int row = color == PlayerColor.White ? 0 : 7;
+            this[row, 0] = new Rook(color);
+            this[row, 1] = new Knight(color);
+            this[row, 2] = new Bishop(color);
+            this[row, 3] = new Queen(color);
+            this[row, 4] = new King(color);
+            this[row, 5] = new Bishop(color);
+            this[row, 6] = new Knight(color);
+            this[row, 7] = new Rook(color);
+        }
+
+        private void AddNonPawnPieces()
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                this[1, col] = new Pawn(PlayerColor.White);
+                this[6, col] = new Pawn(PlayerColor.Black);
+            }
         }
     }
 }
