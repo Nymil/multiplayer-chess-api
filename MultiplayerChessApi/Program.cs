1cf173bc15663d65b6ab33c09c9c1804e2ff5236
Mattice Heyzak <mattice.heyzak@gmail.com>
2025-01-26 11:18:24 +0100

    removed schema filter

--- MultiplayerChessApi/Program.cs
+++ MultiplayerChessApi/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using MultiplayerChessApi.Mapping;
 using MultiplayerChessApi.Middleware;
-using MultiplayerChessApi.SchemaFilters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +12,7 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SchemaFilter<NonNullableSchemaFilter>();
-});
+builder.Services.AddSwaggerGen();
 
 // Register AutoMapper
 builder.Services.AddAutoMapper(typeof(ChessGameProfile));
